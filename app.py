bcce74238d658fcc149838610765737282474835
ostap-skywalker <145613516+ostap-skywalker@users.noreply.github.com>
2026-09-19 01:24:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="UFC AI Rankings", page_icon="🏆", layout="wide")
+st.set_page_config(page_title="UFC ELO Rankings", page_icon="🏆", layout="wide")
 st.title("ELO UFC Rankings")
 st.markdown("Global, Strike, and Grapple Elo ratings")
 
